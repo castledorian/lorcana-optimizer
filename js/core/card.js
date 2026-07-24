476b8eee5649b18dfa2eb6f76fc2f61aa21d3d16
Random <42004582+castledorian@users.noreply.github.com>
2026-07-24 20:35:52 +0200

    Update card.js

--- js/core/card.js
+++ js/core/card.js
@@ -1,1 +1,15 @@
+class Card {
+    constructor(data) {
+        Object.assign(this, data);
+    }
 
+    get displayName() {
+        return `${this.name}${this.version ? ' - ' + this.version : ''}`;
+    }
+
+    get isInkable() {
+        return this.inkwell === true;
+    }
+}
+
+export default Card;
