1fbe73c1b3f9f00c2409cef7eaaaaf948cc29910
Random <42004582+castledorian@users.noreply.github.com>
2026-07-25 16:46:53 +0200

    Add Analyze link to Deck Builder navigation

--- js/views/deck-builder.js
+++ js/views/deck-builder.js
@@ -1,4 +1,3 @@
-// js/views/deck-builder.js
 import { store } from '../store.js';
 import { createCardPool } from '../components/card-pool.js';
 import { createDeckPanel } from '../components/deck-panel.js';
@@ -18,6 +17,7 @@ export function createDeckBuilderView() {
                 <div class="nav-links">
                     <a href="#cards" class="nav-link">Cartes</a>
                     <a href="#deck" class="nav-link active">Deck Builder</a>
+                    <a href="#analyze" class="nav-link">Analyse</a>
                 </div>
             </div>
         </div>
